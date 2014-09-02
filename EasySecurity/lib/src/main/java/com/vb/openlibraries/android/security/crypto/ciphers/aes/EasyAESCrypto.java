0ea33862abbf0c816fdb3e914267401489361af3
Vincent Brison <vincent.brison@gmail.com>
2014-09-02 11:25:35 +0200

    Remove unused ressources. Add documentations. Add copyright.

--- EasySecurity/lib/src/main/java/com/vb/openlibraries/android/security/crypto/ciphers/aes/EasyAESCrypto.java
+++ EasySecurity/lib/src/main/java/com/vb/openlibraries/android/security/crypto/ciphers/aes/EasyAESCrypto.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Vincent Brison.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.vb.openlibraries.android.security.crypto.ciphers.aes;
 
 import com.vb.openlibraries.android.security.crypto.models.CryptedInputStream;
@@ -9,7 +25,10 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Created by Brize on 12/08/2014.
+ * This class provide an easy to use, fast API to do encryption and decryption jobs on Android,
+ * using AES cryptographic tools. This class use the tools provided by {@link com.vb.openlibraries.android.security.crypto.ciphers.aes.AESCrypto}.
+ * Randoms IV are produced through {@link com.vb.openlibraries.android.security.crypto.generators.EasyKeyGenerator} and
+ * send back trough holder in result of encryption methods.
  */
 public abstract class EasyAESCrypto {
 
@@ -20,6 +39,12 @@ public abstract class EasyAESCrypto {
 
     }
 
+    /**
+     * Encrypt data.
+     * @param clearData is the data to encrypt.
+     * @param key is the key used to do the encryption.
+     * @return an holder on the data encrypted and the IV (automatically generated).
+     */
     public static CryptedObject encrypt(byte[] clearData, byte[] key) {
 
         CryptedObject result = new CryptedObject();
@@ -34,6 +59,12 @@ public abstract class EasyAESCrypto {
         return result;
     }
 
+    /**
+     * Decrypt data.
+     * @param cryptedData is the data to decrypt.
+     * @param key is the key used to decrypt data (same used for the encryption).
+     * @return the decrypted data.
+     */
     public static byte[] decrypt(CryptedObject cryptedData, byte[] key) {
 
         // Extract data to decryptInputStream
@@ -42,6 +73,12 @@ public abstract class EasyAESCrypto {
         return clearData;
     }
 
+    /**
+     * Encrypt an output stream, allowing you to directly encrypted what you are writing in a stream.
+     * @param clearData is the based output stream to encrypt.
+     * @param key is the key used to do the encryption.
+     * @return the encrypted output stream and the random IV used, generated through {@link com.vb.openlibraries.android.security.crypto.generators.EasyKeyGenerator}.
+     */
     public static CryptedOutputStream encryptOutputStream(OutputStream clearData, byte[] key) {
 
         CryptedOutputStream result = new CryptedOutputStream();
@@ -56,10 +93,15 @@ public abstract class EasyAESCrypto {
         return result;
     }
 
+    /**
+     * Decrypt an input stream.
+     * @param cryptedData is the input stream to decrypt and the IV used for the encryption.
+     * @param key is the key used to decrypt stream (same used for the encryption).
+     * @return the decrypted stream.
+     */
     public static InputStream decryptInputStream(CryptedInputStream cryptedData, byte[] key) {
 
         // Extract data to decryptInputStream
         return AESCrypto.decryptInputStream(cryptedData.getInputStream(), key, cryptedData.getIV(), DEFAULT_ALGO);
-
     }
 }
